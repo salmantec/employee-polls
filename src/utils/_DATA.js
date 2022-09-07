d82ed47f7461046db8f66a3c81902f69920977b8
Mohamed Salman <mohamed.akj@gmail.com>
2022-09-07 10:41:14 +0530

    Updated _DATA.js file

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -185,34 +185,41 @@ export function _saveQuestion(question) {
   });
 }
 
-export function _saveQuestionAnswer({ authedUser, qid, answer }) {
+export function _saveQuestionAnswer(authedUser, qid, answer) {
   return new Promise((resolve, reject) => {
     if (!authedUser || !qid || !answer) {
       reject("Please provide authedUser, qid, and answer");
     }
-
     setTimeout(() => {
-      users = {
-        ...users,
-        [authedUser]: {
-          ...users[authedUser],
-          answers: {
-            ...users[authedUser].answers,
-            [qid]: answer,
+      try {
+        users = {
+          ...users,
+          [authedUser]: {
+            ...users[authedUser],
+            answers: {
+              ...users[authedUser].answers,
+              [qid]: answer,
+            },
           },
-        },
-      };
+        };
 
-      questions = {
-        ...questions,
-        [qid]: {
-          ...questions[qid],
-          [answer]: {
-            ...questions[qid][answer],
-            votes: questions[qid][answer].votes.concat([authedUser]),
+        questions = {
+          ...questions,
+          [qid]: {
+            ...questions[qid],
+            [answer]: {
+              ...questions[qid][answer],
+              votes: questions[qid][answer].votes.concat([authedUser]),
+            },
           },
-        },
-      };
+        };
+      } catch (e) {
+        //Redux state management is not going to work at this level
+        console.info(
+          "New users are not saved to real database, current users in _Data are : ",
+          Object.keys(users)
+        );
+      }
 
       resolve(true);
     }, 500);
